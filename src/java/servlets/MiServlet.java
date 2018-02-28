cb3e0bfe8d649774dcc8a884204ca346a68e7237
Javier Fernández García <javier.fernandez3@live.u-tad.com>
2018-02-28 21:15:37 +0100

    Diferencia entre leer y buscar

--- src/java/servlets/MiServlet.java
+++ src/java/servlets/MiServlet.java
@@ -6,8 +6,10 @@
 package servlets;
 
 import beans.session;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import static java.net.Proxy.Type.HTTP;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,6 +18,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import modelo.Vuelos;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 
 /**
@@ -42,19 +47,94 @@ public class MiServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<html>");
+            /*out.println("<html>");
             out.println("<head>");
             out.println(
-            "<title>Prueba Servlet con EJB </title >");
+            "<title>Prueba Servlet con EJB hterfghgfjjfj</title >");
  out.println("</head>");
-            out.println("<body>");
-            List<Vuelos> l = aEJB.findAll();
-            /* bucle para recorrer la lista que corresponda */
+            out.println("<body>");*/
+            //List<Vuelos> l = aEJB.findAll();
+            
+            String origen;
+            String destino;
+            String fecha;
+            
+            StringBuffer jb = new StringBuffer();
+  String line = null;
+  try {
+    BufferedReader reader = request.getReader();
+    while ((line = reader.readLine()) != null)
+      jb.append(line);
+  } catch (Exception e) { /*report an error*/ }
+
+  
+    //JSONObject jsonObject = HTTP.    HTTP.toJSONObject(jb.toString());
+  
+            //out.println(jb.toString());
+  
+            JSONObject peticionjson = (JSONObject) JSONValue.parse(jb.toString());
+            //out.println(peticionjson.toJSONString());
+            String peticion = peticionjson.get("peticion").toString();
+            
+            //out.println(peticion);
+            
+            if(peticion.equals("buscar")){
+                
+            
+              
+                      JSONObject vuelos = (JSONObject) peticionjson.get("vueloBuscar");
+            
+            //out.println(vuelos);
+                      
+            
+            
+            origen=(String)vuelos.get("origen");
+            destino=(String)vuelos.get("destino");
+            fecha=(String)vuelos.get("fecha");
+            
+            //out.println(origen +"  "+ destino +"  "+ fecha);
+            
+            List<Vuelos> l = aEJB.findByCriteria(origen,destino,fecha);
+            
+            // bucle para recorrer la lista que corresponda 
+            JSONArray arrayPadre = new JSONArray();
             for (int i = 0; i < l.size(); i++) {
-                out.println("<b>Origen:</b>" + l.get(i).getOrigen()+ ", <b>Destino </b>" + l.get(i).getDestino() + ", <b>Precio </b>" + l.get(i).getPrecio()+ "<br>");
+                //out.println("<b>Origen:</b>" + l.get(i).getOrigen()+ ", <b>Destino </b>" + l.get(i).getDestino() + ", <b>Precio </b>" + l.get(i).getPrecio()+ "<br>");
+                
+                JSONObject jsonHijo = new JSONObject();
+                jsonHijo.put("idVuelo", l.get(i).getIdVuelo());
+                jsonHijo.put("diayhora", l.get(i).getDiayhora());
+                jsonHijo.put("origen", l.get(i).getOrigen());
+                jsonHijo.put("destino", l.get(i).getDestino());
+                jsonHijo.put("precio", l.get(i).getPrecio());
+                jsonHijo.put("plazas_totales", l.get(i).getPlazasTotales());
+                jsonHijo.put("plazas_libres", l.get(i).getPlazasLibres());
+                
+             arrayPadre.add(jsonHijo);
             }
-            out.println("</body>");
-            out.println("</html>");
+            out.println(arrayPadre.toJSONString());
+                  
+            }else if(peticion.equals("leer")){
+                List<Vuelos> l = aEJB.findAll();
+                JSONArray arrayPadre = new JSONArray();
+            for (int i = 0; i < l.size(); i++) {
+                //out.println("<b>Origen:</b>" + l.get(i).getOrigen()+ ", <b>Destino </b>" + l.get(i).getDestino() + ", <b>Precio </b>" + l.get(i).getPrecio()+ "<br>");
+                
+                JSONObject jsonHijo = new JSONObject();
+                jsonHijo.put("idVuelo", l.get(i).getIdVuelo());
+                jsonHijo.put("diayhora", l.get(i).getDiayhora());
+                jsonHijo.put("origen", l.get(i).getOrigen());
+                jsonHijo.put("destino", l.get(i).getDestino());
+                jsonHijo.put("precio", l.get(i).getPrecio());
+                jsonHijo.put("plazas_totales", l.get(i).getPlazasTotales());
+                jsonHijo.put("plazas_libres", l.get(i).getPlazasLibres());
+                
+             arrayPadre.add(jsonHijo);
+            }
+            out.println(arrayPadre.toJSONString());
+            }
+            /*out.println("</body>");
+            out.println("</html>");*/
         }
     }
 
